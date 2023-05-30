8b8662cc34f442dc9e74b83f362861324543901b
Neto test <o.sirotina@inbox.ru>
2023-05-30 15:40:26 +0300

    added tests

--- src/main/java/MoviesManager.java
+++ src/main/java/MoviesManager.java
@@ -1,5 +1,14 @@
 public class MoviesManager {
     private String[] movies = new String[0];
+    private int limit;
+
+    public MoviesManager(int limit) {
+        this.limit = limit;
+    }
+
+    public MoviesManager() {
+        this.limit = 5;
+    }
 
     public void add(String movie) {
         String[] tmp = new String[movies.length + 1];
@@ -16,22 +25,17 @@ public class MoviesManager {
 
     public String[] findLast() {
         int resultLength;
-        if (movies.length < 10) {
+        if (movies.length < limit) {
             resultLength = movies.length;
         } else {
-            resultLength = 10;
+            resultLength = limit;
         }
-        String[] tmp = new String[10];
+        String[] tmp = new String[resultLength];
         for (int i = 0; i < tmp.length; i++) {
             tmp[i] = movies[movies.length - 1 - i];
 
         }
         return tmp;
 
-
     }
-
 }
-
-
-
